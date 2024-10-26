ceaf8a09ef795528d3ca0fb26511eff37a1c6083
ShodiyevF <fayzulloh6757@gmail.com>
2024-10-26 16:23:36 +0500

    complete db schema

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,6 +1,7 @@
-import { json, pgTable, smallint, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import { boolean, integer, json, jsonb, pgTable, smallint, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 
 const requestTypes: [string, ...string[]] = [ "SUCCESS", "ERROR" ]
+const apiMethods: [string, ...string[]] = ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS", "CONNECT", "TRACE"]
 
 export const requestsLOGSTable = pgTable('requests_logs', {
     requestId: uuid('request_id').defaultRandom().primaryKey(),
@@ -26,6 +27,53 @@ export const cronJobsLOGSTable = pgTable('cron_jobs_logs', {
     cjId: uuid('cj_id').defaultRandom().primaryKey(),
     cjName: text('cj_name').notNull(),
     cjCreatedAt: timestamp('cj_created_at').notNull().defaultNow(),
+})
+
+
+export const usersTable = pgTable('users', {
+    userId: uuid('user_id').defaultRandom().primaryKey(),
+    userFirstName: varchar('user_first_name', { length: 32 }).notNull(),
+    userLastName: varchar('user_last_name', { length: 32 }).notNull(),
+    userEmail: varchar('user_email', { length: 64 }).notNull(),
+    userPassword: varchar('user_password', { length: 64 }).notNull(),
+    userCreatedAt: timestamp('user_created_at').notNull().defaultNow(),
+})
+
+export const projectsTable = pgTable('projects', {
+    projectId: uuid('project_id').defaultRandom().primaryKey(),
+    projectFirstName: varchar('project_full_name', { length: 32 }).notNull(),
+    projectDescription: text('project_description'),
+    projectOwnerId: uuid('project_owner_id').notNull().references(() => usersTable.userId),
+    projectCreatedAt: timestamp('project_created_at').notNull().defaultNow(),
+})
+
+export const apisTable = pgTable('apis', {
+    apiId: uuid('api_id').defaultRandom().primaryKey(),
+    apiRoute: varchar('api_route', { length: 128 }).notNull(),
+    apiMethod: varchar('api_method', { enum: apiMethods }).notNull(),
+    apiOwnerId: uuid('api_owner_id').notNull().references(() => usersTable.userId),
+    apiCreatedAt: timestamp('api_created_at').notNull().defaultNow(),
+})
+
+export const responsesTable = pgTable('responses', {
+    responseId: uuid('response_id').defaultRandom().primaryKey(),
+    responseStatus: boolean('response_status').notNull(),
+    responseStatusCode: integer('response_status_code').notNull(),
+    responseDescription: text('response_description'),
+    responseKeys: jsonb('response_keys').notNull(),
+    responseOwnerId: uuid('response_owner_id').notNull().references(() => usersTable.userId),
+    responseApiId: uuid('response_api_id').notNull().references(() => apisTable.apiId),
+    responseCreatedAt: timestamp('response_created_at').notNull().defaultNow(),
+})
+
+export const payloadsTable = pgTable('payloads', {
+    payloadId: uuid('payload_id').defaultRandom().primaryKey(),
+    payloadType: varchar('payload_type', { length: 6 }).notNull(),
+    payloadDescription: text('payload_description'),
+    payloadKeys: jsonb('payload_keys').notNull(),
+    payloadOwnerId: uuid('payload_owner_id').notNull().references(() => usersTable.userId),
+    payloadApiId: uuid('payload_api_id').notNull().references(() => apisTable.apiId),
+    payloadCreatedAt: timestamp('payload_created_at').notNull().defaultNow(),
 })
 
 namespace DbTableSchema {
