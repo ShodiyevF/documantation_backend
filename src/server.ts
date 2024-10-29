53fa9c0b0bfa53651e55a25cf8e3951d26a92c38
ShodiyevF <fayzulloh6757@gmail.com>
2024-10-29 18:14:11 +0500

    add apis to server

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,6 @@ import app from './app'
 
 import projects from './module/projects/projects.index'
 import auth from './module/auth/auth.index'
+import apis from './module/apis/apis.index'
 
-app([auth, projects]);
+app([auth, projects, apis]);
