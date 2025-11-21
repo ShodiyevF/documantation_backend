fc89f95c4e10f3e882b0640c4a622eb7fd9cd389
Fayzulloh <fayzulloh6757@gmail.com>
2025-11-21 11:45:54 +0500

    add timezones and is deleted

--- src/database/schema.database.ts
+++ src/database/schema.database.ts
@@ -18,20 +18,20 @@ export const requestsLOGSTable = pgTable('requests_logs', {
     rlBody: json('rl_body').notNull(),
     rlResponseStatus: smallint('rl_response_status').notNull(),
     rlResponseBody: text('rl_response_body').notNull(),
-    rlCreatedAt: timestamp('rl_created_at').notNull().defaultNow(),
+    rlCreatedAt: timestamp('rl_created_at', { withTimezone: true }).notNull().defaultNow(),
 })
 
 export const internalErrorsLOGSTable = pgTable('internal_errors_logs', {
     ielId: uuid('iel_id').defaultRandom().primaryKey(),
     ielDescription: text('iel_description').notNull(),
     ielStack: text('iel_stack').notNull(),
-    ielCreatedAt: timestamp('iel_created_at').notNull().defaultNow(),
+    ielCreatedAt: timestamp('iel_created_at', { withTimezone: true }).notNull().defaultNow(),
 })
 
 export const cronJobsLOGSTable = pgTable('cron_jobs_logs', {
     cjlId: uuid('cjl_id').defaultRandom().primaryKey(),
     cjlName: text('cjl_name').notNull(),
-    cjlCreatedAt: timestamp('cjl_created_at').notNull().defaultNow(),
+    cjlCreatedAt: timestamp('cjl_created_at', { withTimezone: true }).notNull().defaultNow(),
 })
 
 
@@ -41,7 +41,7 @@ export const usersTable = pgTable('users', {
     userLastName: varchar('user_last_name', { length: 32 }).notNull(),
     userEmail: varchar('user_email', { length: 64 }).notNull(),
     userPassword: varchar('user_password', { length: 64 }).notNull(),
-    userCreatedAt: timestamp('user_created_at').notNull().defaultNow(),
+    userCreatedAt: timestamp('user_created_at', { withTimezone: true }).notNull().defaultNow(),
 }, table => ({
     uniqueUserEmail: unique().on(table.userEmail)
 }))
@@ -52,27 +52,25 @@ export const projectsTable = pgTable('projects', {
     projectBaseUrl: varchar('project_base_url', { length: 128 }),
     projectAuthorization: projectsProjectAuthorizationTypeEnum('project_authorization_type').notNull(),
     projectDescription: text('project_description'),
+    projectIsDeleted: boolean('project_is_deleted').notNull().default(false),
     projectOwnerId: uuid('project_owner_id').notNull().references(() => usersTable.userId),
-    projectCreatedAt: timestamp('project_created_at').notNull().defaultNow(),
-}, table => ({
-    uniqueProjectName: unique().on(table.projectName, table.projectOwnerId)
-}))
+    projectCreatedAt: timestamp('project_created_at', { withTimezone: true }).notNull().defaultNow(),
+})
 
 export const projectInvitationsTable = pgTable('project_invitations', {
     piId: uuid('pi_id').defaultRandom().primaryKey(),
     piAccepted: boolean('pi_accepted').notNull().default(false),
+    piIsDeleted: boolean('pi_is_deleted').notNull().default(false),
     piProjectId: uuid('pi_project_id').notNull().references(() => projectsTable.projectId),
     piUserId: uuid('pi_user_id').notNull().references(() => usersTable.userId),
-    piCreatedAt: timestamp('pi_created_at').notNull().defaultNow(),
-}, table => ({
-    uniqueProjectUser: unique().on(table.piProjectId, table.piUserId)
-}))
+    piCreatedAt: timestamp('pi_created_at', { withTimezone: true }).notNull().defaultNow(),
+})
 
 export const projectUsersTable = pgTable('project_users', {
     puId: uuid('pu_id').defaultRandom().primaryKey(),
     puProjectId: uuid('pu_project_id').notNull().references(() => projectsTable.projectId),
     puUserId: uuid('pu_user_id').notNull().references(() => usersTable.userId),
-    puCreatedAt: timestamp('pu_created_at').notNull().defaultNow(),
+    puCreatedAt: timestamp('pu_created_at', { withTimezone: true }).notNull().defaultNow(),
 }, table => ({
     uniqueProjectUser: unique().on(table.puProjectId, table.puUserId)
 }))
@@ -81,9 +79,10 @@ export const codesTable = pgTable('codes', {
     codeId: uuid('code_id').defaultRandom().primaryKey(),
     codeValue: varchar('code_value', { length: 64 }).notNull(),
     codeDescription: varchar('code_description', { length: 128 }).notNull(),
+    codeIsDeleted: boolean('code_is_deleted').notNull().default(false),
     codeUserId: uuid('code_user_id').notNull().references(() => usersTable.userId),
     codeProjectId: uuid('code_project_id').notNull().references(() => projectsTable.projectId),
-    codeCreatedAt: timestamp('code_created_at').notNull().defaultNow(),
+    codeCreatedAt: timestamp('code_created_at', { withTimezone: true }).notNull().defaultNow(),
 }, table => ({
     uniqueProjectUser: unique().on(table.codeValue, table.codeProjectId)
 }))
@@ -92,12 +91,11 @@ export const modulesTable = pgTable('modules', {
     moduleId: uuid('module_id').defaultRandom().primaryKey(),
     moduleName: varchar('module_name', { length: 32 }).notNull(),
     moduleDescription: text('module_description'),
+    moduleIsDeleted: boolean('module_is_deleted').notNull().default(false),
     moduleOwnerId: uuid('module_owner_id').notNull().references(() => usersTable.userId),
     moduleProjectId: uuid('module_project_id').notNull().references(() => projectsTable.projectId),
-    moduleCreatedAt: timestamp('module_created_at').notNull().defaultNow(),
-}, table => ({
-    uniqueModuleName: unique().on(table.moduleName, table.moduleProjectId)
-}))
+    moduleCreatedAt: timestamp('module_created_at', { withTimezone: true }).notNull().defaultNow(),
+})
 
 export const apisTable = pgTable('apis', {
     apiId: uuid('api_id').defaultRandom().primaryKey(),
@@ -106,10 +104,11 @@ export const apisTable = pgTable('apis', {
     apiMethod: apisApiMethodEnum('api_method').notNull(),
     apiAuthorization: boolean('api_authorization').notNull(),
     apiDescription: varchar('api_description', { length: 128 }),
+    apiIsDeleted: boolean('api_is_deleted').notNull().default(false),
     apiOwnerId: uuid('api_owner_id').notNull().references(() => usersTable.userId),
     apiModuleId: uuid('api_module_id').notNull().references(() => modulesTable.moduleId),
     apiProjectId: uuid('api_project_id').notNull().references(() => projectsTable.projectId),
-    apiCreatedAt: timestamp('api_created_at').notNull().defaultNow(),
+    apiCreatedAt: timestamp('api_created_at', { withTimezone: true }).notNull().defaultNow(),
 }, table => ({
     uniqueApiRoute: unique().on(table.apiRoute, table.apiMethod, table.apiProjectId)
 }))
@@ -120,9 +119,17 @@ export const payloadsTable = pgTable('payloads', {
     payloadSchema: jsonb('payload_schema').notNull(),
     payloadExample: text('payload_example'),
     payloadDescription: varchar('payload_description', { length: 128 }),
+    payloadIsDeleted: boolean('payload_is_deleted').notNull().default(false),
     payloadOwnerId: uuid('payload_owner_id').notNull().references(() => usersTable.userId),
     payloadApiId: uuid('payload_api_id').notNull().references(() => apisTable.apiId),
-    payloadCreatedAt: timestamp('payload_created_at').notNull().defaultNow(),
+    payloadCreatedAt: timestamp('payload_created_at', { withTimezone: true }).notNull().defaultNow(),
+})
+
+export const payloadCodesTable = pgTable('payload_codes', {
+    pcId: uuid('pc_id').defaultRandom().primaryKey(),
+    pcPayloadId: uuid('pc_payload_id').notNull().references(() => payloadsTable.payloadId),
+    pcCodeId: uuid('pc_code_id').notNull().references(() => codesTable.codeId),
+    pcCreatedAt: timestamp('pc_created_at', { withTimezone: true }).notNull().defaultNow(),
 })
 
 export const responsesTable = pgTable('responses', {
@@ -131,9 +138,17 @@ export const responsesTable = pgTable('responses', {
     responseSchema: jsonb('response_schema').notNull(),
     responseExample: text('response_example'),
     responseDescription: varchar('response_description', { length: 128 }),
+    responseIsDeleted: boolean('response_is_deleted').notNull().default(false),
     responseOwnerId: uuid('response_owner_id').notNull().references(() => usersTable.userId),
     responseApiId: uuid('response_api_id').notNull().references(() => apisTable.apiId),
-    responseCreatedAt: timestamp('response_created_at').notNull().defaultNow(),
+    responseCreatedAt: timestamp('response_created_at', { withTimezone: true }).notNull().defaultNow(),
+})
+
+export const responseCodesTable = pgTable('response_codes', {
+    rcId: uuid('rc_id').defaultRandom().primaryKey(),
+    rcResponseId: uuid('rc_response_id').notNull().references(() => responsesTable.responseId),
+    rcCodeId: uuid('rc_code_id').notNull().references(() => codesTable.codeId),
+    rcCreatedAt: timestamp('rc_created_at', { withTimezone: true }).notNull().defaultNow(),
 })
 
 namespace DbTableSchema {
