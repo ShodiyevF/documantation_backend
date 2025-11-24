d435278f78ac23ccbff2c85147b012d40849520a
Fayzulloh <fayzulloh6757@gmail.com>
2025-11-24 20:49:40 +0500

    add code owner on get codes

--- src/query/codes.query.ts
+++ src/query/codes.query.ts
@@ -6,8 +6,14 @@ import { db } from "@database/pg.database"
 
 namespace CodesQuery {
 
-    export async function getCodeById(codeId: string) {
-        return await db.select({
+    export async function getCodes(payloads: CodesInterface.IGetCodesPayloads) {
+        const {
+            limit,
+            page,
+            projectId
+        } = payloads
+
+        const data = await db.select({
             code_id: DbTableSchema.codes.codeId,
             code_value: DbTableSchema.codes.codeValue,
             code_description: DbTableSchema.codes.codeDescription,
@@ -21,26 +27,6 @@ namespace CodesQuery {
         })
         .from(DbTableSchema.codes)
         .leftJoin(DbTableSchema.users, eq(DbTableSchema.users.userId, DbTableSchema.codes.codeUserId))
-        .where(
-            eq(DbTableSchema.codes.codeId, codeId)
-        )
-        .then(data => data[0])
-    }
-
-    export async function getCodes(payloads: CodesInterface.IGetCodesPayloads) {
-        const {
-            limit,
-            page,
-            projectId
-        } = payloads
-
-        const data = await db.select({
-            code_id: DbTableSchema.codes.codeId,
-            code_value: DbTableSchema.codes.codeValue,
-            code_description: DbTableSchema.codes.codeDescription,
-            code_created_at: DbTableSchema.codes.codeCreatedAt,
-        })
-        .from(DbTableSchema.codes)
         .where(
             and(
                 eq(DbTableSchema.codes.codeIsDeleted, false),
@@ -69,6 +55,26 @@ namespace CodesQuery {
         }
     }
     
+    export async function getCodeById(codeId: string) {
+        return await db.select({
+            code_id: DbTableSchema.codes.codeId,
+            code_value: DbTableSchema.codes.codeValue,
+            code_description: DbTableSchema.codes.codeDescription,
+            code_creator: {
+                user_id: DbTableSchema.users.userId,
+                user_first_name: DbTableSchema.users.userFirstName,
+                user_last_name: DbTableSchema.users.userLastName,
+                user_email: DbTableSchema.users.userEmail,
+            },
+            code_created_at: DbTableSchema.codes.codeCreatedAt,
+        })
+        .from(DbTableSchema.codes)
+        .leftJoin(DbTableSchema.users, eq(DbTableSchema.users.userId, DbTableSchema.codes.codeUserId))
+        .where(
+            eq(DbTableSchema.codes.codeId, codeId)
+        )
+        .then(data => data[0])
+    }
 }
 
 export default CodesQuery
