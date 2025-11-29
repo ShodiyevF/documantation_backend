c743d698cf61e9e1cdfe59d3a36401579c08d363
Fayzulloh <fayzulloh6757@gmail.com>
2025-11-29 23:52:38 +0500

    add is valid regex

--- src/util/regex.util.ts
+++ src/util/regex.util.ts
@@ -1,6 +1,15 @@
 namespace RegexUtil {
     export const UUID = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
     export const email = /^(?=.{1,64}$)[\w.-]+@([\w-]+\.)+[\w-]{2,4}$/
+    
+    export function isValidRegex(pattern: RegExp) {
+        try {
+            new RegExp(pattern);
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
 }
 
 export default RegexUtil
